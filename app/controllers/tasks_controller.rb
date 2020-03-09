10e4d5ea33cb2756da4f4b87923f2d7c59a7ea28
yoshinoriki <ec2-user@ip-172-31-81-8.ec2.internal>
2020-03-09 08:43:28 +0000

    add private and modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,6 +47,8 @@ class TasksController < ApplicationController
       redirect_to tasks_url
     end
     
+    private
+    
     #Strong Parameter
     def task_params
       params.require(:task).permit(:content)
