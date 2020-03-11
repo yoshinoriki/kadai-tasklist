44cdc738f3fef153d925dce1db4ac8901065f4d2
yoshinoriki <ec2-user@ip-172-31-81-8.ec2.internal>
2020-03-11 11:13:01 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'tasks#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
-
   resources :users, only: [:create, :new,]
   resources :tasks
 end
