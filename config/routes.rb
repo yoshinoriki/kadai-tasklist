fb586b9a69a33efa7f52bc9af6e77e6d30a55887
yoshinoriki <ec2-user@ip-172-31-81-8.ec2.internal>
2020-03-11 09:14:44 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
   
+  get 'signup', to: 'users#new'
+
+  resources :users, only: [:create, :new,]
   resources :tasks
 end
