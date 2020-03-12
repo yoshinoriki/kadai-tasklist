20edd35e57c01af85be21ec636c476c65f6d503b
yoshinoriki <ec2-user@ip-172-31-81-8.ec2.internal>
2020-03-12 09:11:02 +0000

    add redirect login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,8 @@ class TasksController < ApplicationController
     def index
       if logged_in?
         @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      else
+        redirect_to login_url
       end
     end
     
